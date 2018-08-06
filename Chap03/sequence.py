3df055213497fac48ced1785ea0976a51404969d
solomonsokoya <solomonsokoya718@gmail.com>
2018-08-06 17:26:09 -0400

    dictionaries

--- Chap03/sequence.py
+++ Chap03/sequence.py
@@ -2,5 +2,7 @@
 # Copyright 2009-2017 BHG http://bw.org/
 
 x = [ 1, 2, 3, 4, 5 ]
-for i in x:
-    print('i is {}'.format(i))
+x1 = {'one': 1, 'two': 2, 'three': 3}
+
+for k, v in x1.items():
+    print('key: {}, value: {}'.format(k,v))
--- Chap03/types.py
+++ Chap03/types.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
+# from decimal import *
+#
+# a = Decimal('.10')
+# b = Decimal('.30')
+
 x = 7
 print('x is {}'.format(x))
 print(type(x))
